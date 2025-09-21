8f9fb1661b0b6aa61427b0fb520549fdc0ff9840
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2025-09-22 00:42:23 +0700

    add models

--- matching/models.py
+++ matching/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class MutualPlaylist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Swipe(models.Model):
+    swiper = models.ForeignKey(User, on_delete=models.CASCADE, related_name='swiper')
+    swiped = models.ForeignKey(User, on_delete=models.CASCADE, related_name='swiped')
+    action = models.CharField(max_length=255)
+    swiped_at = models.DateTimeField(auto_now_add=True)
+
+
+class Match(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='match_user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='match_user2')
+    similarity_score = models.FloatField()
+    matched_at = models.DateTimeField(auto_now_add=True)
+    mutual_playlist = models.ForeignKey(MutualPlaylist, on_delete=models.SET_NULL, null=True, blank=True)
+
+class Chat(models.Model):
+    match = models.OneToOneField(Match, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    message_type = models.CharField(max_length=255)
+    shared_item_id = models.IntegerField(blank=True, null=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    spotify_id = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    profile_picture = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
